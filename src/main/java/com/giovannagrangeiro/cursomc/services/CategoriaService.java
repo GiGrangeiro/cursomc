d0d3c98940ec326c48827daac7d44f48e588fd33
Giovanna Grangeiro <giiigrangvanderlei1211@hotmail.com>
2023-04-18 10:19:02 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/giovannagrangeiro/cursomc/services/CategoriaService.java
@@ -0,0 +1,24 @@
+package com.giovannagrangeiro.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.giovannagrangeiro.cursomc.domain.Categoria;
+import com.giovannagrangeiro.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		 Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+		}  
+		
+	}
+
+
