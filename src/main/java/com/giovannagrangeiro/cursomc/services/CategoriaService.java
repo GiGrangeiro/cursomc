458260da3728aabc71b74b1a2cd73bc0b432ffdd
Giovanna Grangeiro <giiigrangvanderlei1211@hotmail.com>
2023-04-24 11:08:09 -0300

    Ajustes nos endpoint /catgorias/{id}

--- src/main/java/com/giovannagrangeiro/cursomc/services/CategoriaService.java
+++ src/main/java/com/giovannagrangeiro/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.giovannagrangeiro.cursomc.domain.Categoria;
 import com.giovannagrangeiro.cursomc.repositories.CategoriaRepository;
+import com.giovannagrangeiro.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,8 +17,11 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		 Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-		}  
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		} 
+
+
 		
 	}
 
