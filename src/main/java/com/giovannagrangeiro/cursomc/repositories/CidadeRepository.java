35318b106c5ec386b7bc3093178a4f8c231dfa03
Giovanna Grangeiro <giiigrangvanderlei1211@hotmail.com>
2023-04-25 13:42:43 -0300

    Estado e Cidade

---
+++ src/main/java/com/giovannagrangeiro/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.giovannagrangeiro.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.giovannagrangeiro.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> 	{
+
+}
---
+++ src/main/java/com/giovannagrangeiro/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.giovannagrangeiro.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.giovannagrangeiro.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> 	{
+
+}
