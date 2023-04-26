fefaf01ca656e99fd060f233a3c0d748feed32da
Giovanna Grangeiro <giiigrangvanderlei1211@hotmail.com>
2023-04-25 21:16:13 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/giovannagrangeiro/cursomc/domain/Cliente.java
+++ src/main/java/com/giovannagrangeiro/cursomc/domain/Cliente.java
@@ -38,6 +38,9 @@ public class Cliente implements Serializable{
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	@OneToMany(mappedBy="cliente" )
+	private List<Pedido> pedidos = new ArrayList<>();	
+	
 	public Cliente() {
 		
 	}
@@ -86,24 +89,6 @@ public class Cliente implements Serializable{
 	public TipoCliente getTipo() {
 		return TipoCliente.toEnum(tipo);
 	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Cliente other = (Cliente) obj;
-		return Objects.equals(id, other.id);
-	}
-
 	public void setTipo(TipoCliente tipo) {
 		this.tipo = tipo.getCod();
 	}
@@ -123,6 +108,34 @@ public class Cliente implements Serializable{
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	
 	
 	
 }
